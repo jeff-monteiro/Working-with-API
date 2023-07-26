465ad741969423a4faa2d4bf7c795054deef0ea5
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2023-07-26 12:59:30 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro) {
+        throw Error('Esse CEP não existe')
+    } else
+    console.log(r)
+})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log("Processo finalizado!"))
+
+console.log(consultaCEP)
