5768f72b044dfbf92f919d1cc9bdc76dfe6c24cd
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2023-08-10 00:40:24 -0300

    using_map_method_to_array

---
+++ Array-JS/alura_books-projeto_inicial/app/mapMethod.js
@@ -0,0 +1,7 @@
+function applyDiscounts(books){
+    const discount = 0.3
+    discountBooks = books.map(book => {
+        return {...listBooks}
+    })
+
+}
--- Array-JS/alura_books-projeto_inicial/app/script.js
+++ Array-JS/alura_books-projeto_inicial/app/script.js
@@ -1,28 +1,10 @@
-const booksTable = document.getElementById("livros")
 searchBooks()
 async function searchBooks(){
     const bookShelf = await fetch('https://guilhermeonrails.github.io/casadocodigo/livros.json')
     const listBooks = await bookShelf.json()
-    console.table(listBooks)
+    let discountBooks = applyDiscounts(listBooks)
+    //console.table(listBooks)
     
-    showBooksOnScreen(listBooks)
-}
-
-function showBooksOnScreen(listOfBooks){
-    listOfBooks.forEach(livro => {
-        booksTable.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco + "0"}</p>
-            <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-            </div>
-        </div>
-        `
-    })
+    showBooksOnScreen(discountBooks)
 }
 
