fd53af6b0dff5566001e7ab0024061688f8d55c4
Jefferson <jeffmonteiro61@icloud.com>
2023-10-08 19:55:52 -0300

    usingMapMethod

--- Array-JS/alura_books-projeto_inicial/app/script.js
+++ Array-JS/alura_books-projeto_inicial/app/script.js
@@ -2,26 +2,8 @@ searchBooks()
 async function searchBooks(){
     const bookShelf = await fetch('https://guilhermeonrails.github.io/casadocodigo/livros.json')
     const listBooks = await bookShelf.json()
-    console.table(listBooks)
+    let booksWithDiscount = 
     
     showBooksOnScreen(listBooks)
-}
-
-function showBooksOnScreen(listOfBooks){
-    listOfBooks.forEach(livro => {
-        booksTable.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco + "0"}</p>
-            <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-            </div>
-        </div>
-        `
-    })
 }
 
--- Array-JS/notations.md
+++ Array-JS/notations.md
@@ -1,4 +1,4 @@
 ### Notations about Javascript Arrays Method
 
 -> forEach => Itera em cada uma das posições do Array ou Objeto.
--> Map: Array Modificado => 
+-> Map: Array Modificado => Não altera o array original, sempre retornando um valor.
